33de04b5468f289b511f5341bf3b3cffe6bf19b3
Anabella Lamour <ana.lamour@outlook.com>
2022-10-11 21:43:42 -0300

    listado base de datos

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-
+# conección Mysql 
 app.config ["MYSQL_HOST"] = 'localhost'
 app.config ["MYSQL_USSER"] = 'root'
-app.config ["MYSQL_PASSWORD"] = '123'
+app.config ["MYSQL_PASSWORD"] = ''
 app.config ["MYSQL_DB"] = 'flaskcontacts'
-
-
 mysql = MySQL(app)
+
+
+#Inicio Sesión
+app.secret_key = 'mysecretkey'
 
 @app.route("/")
 def Index():
-    return render_template("index.html")
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM contacts')
+    data = cur.fetchall()
+    return render_template('index.html', contacts = data)
 
 @app.route("/add_contact", methods=['POST'])
 def add_contact():
@@ -25,7 +30,9 @@ def add_contact():
         cur = mysql.connection.cursor()
         cur.execute('INSERT INTO contacts (fullname, phone,email) VALUES (%s, %s, %s)',(fullname, phone, email))
         mysql.connection.commit ()
+        flash("Contacto agregado")
         return redirect(url_for('Index'))
+        
 
 
 @app.route("/edit")
